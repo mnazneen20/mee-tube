08c4d7655e42eacb85d2bc8a03f2ea9defba2929
unknown <masumanzneen2019@gmail.com>
2023-02-22 19:37:42 +0600

    updating env file

--- src/Requests.js
+++ src/Requests.js
@@ -2,7 +2,7 @@ const baseurl = 'https://youtube-v3-alternative.p.rapidapi.com/'
 const options = {
             method: 'GET',
             headers: {
-                'X-RapidAPI-Key': '46c2c9db67msh470b8fbad84e310p1cf7fejsndf817e3d70d9',
+                'X-RapidAPI-Key': `${process.env.REACT_APP_RAPID_API_KEY}`,
                 'X-RapidAPI-Host': 'youtube-v3-alternative.p.rapidapi.com'
             }
         };
